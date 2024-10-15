2953efb5bfe1c27a2adcd0072cd4ecb66fd4c3ba
AmaruEscalante <amaruescalante@gmail.com>
2024-10-15 14:14:21 -0700

    feat: center the tildeheader v2

--- src/views/v2/TildeHeaderV2.tsx
+++ src/views/v2/TildeHeaderV2.tsx
@@ -15,21 +15,21 @@ const TildeHeaderV2: React.FC<TildeHeaderV2Props> = ({
   enterprises = 0,
 }) => {
   return (
-    <div className="flex w-[300px] justify-between bg-[#FAFAFA] rounded-full text-black text-sm px-3 border-2 border-[#CBD2E0]">
-      <span className="flex text-2xl">~</span>
-      <div className="flex items-center border-l-2 border-[#CBD2E0] pl-3">
+    <div className="flex w-full justify-between bg-[#FAFAFA] rounded-full text-black text-sm px-3 border-2 border-[#CBD2E0]">
+      <span className="flex text-2xl pr-2">~</span>
+      <div className="flex justify-center items-center border-l-2 border-[#CBD2E0] pl-3 w-full">
         <span className="text-blue-500">
           <MdLayers />
         </span>
         <span className="ml-1">{datapoints}</span>
       </div>
-      <div className="flex items-center border-l-2 border-[#CBD2E0] pl-3">
+      <div className="flex justify-center items-center border-l-2 border-[#CBD2E0] pl-3 w-full">
         <span className="text-purple-500">
           <CoinIcon />
         </span>
         <span className="ml-1">{potentialValue?.toFixed(2)}</span>
       </div>
-      <div className="flex items-center border-l-2 border-[#CBD2E0] pl-3">
+      <div className="flex justify-center items-center border-l-2 border-[#CBD2E0] pl-3 w-full">
         <span className="text-gray-500">
           <FaBriefcase size={15} />
         </span>
